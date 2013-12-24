654cab1048df8508048369d5e87bb2f15df4cda5
phantichhoang <phantichhoang@4df6940f-bb5a-25f8-e46e-23d9f5565ca8>
2013-12-24 07:35:06 +0000

    ebook

---
+++ trunk/hoangpt/2013-13-24/Tutor0201_helloandroid/src/com/hoangphan/tutor0201_helloandroid/MainActivity.java
@@ -0,0 +1,49 @@
+package com.hoangphan.tutor0201_helloandroid;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+		TextView helloLbl;
+		EditText nameTxt;
+		
+		class NameListener implements View.OnKeyListener {
+        	public boolean onKey(View view, int keyCode, KeyEvent event){
+        		//if enterkey, display text on say hello
+        		if(KeyEvent.ACTION_DOWN == event.getAction() && KeyEvent.KEYCODE_ENTER == keyCode){
+        			String name = nameTxt.getText().toString();
+        			helloLbl.setText("Nice to meet you, "+name+".");//say
+        			nameTxt.setText(""); //reset
+        			return true;
+        		} else {
+	        		return false;
+        		}
+        	}
+		}
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setTitle("Hello Android World");
+        
+        //init layout
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        
+        //add component, using code to build ui
+        helloLbl = new TextView(this);
+        helloLbl.setText("Welcome to Android World");
+        layout.addView(helloLbl);
+        
+        nameTxt = new EditText(this);
+        nameTxt.setHint("Enter your name");
+        nameTxt.setLines(1);
+        layout.addView(nameTxt);
+        
+        nameTxt.setOnKeyListener(new NameListener());
+        setContentView(layout);
+    }
+}
