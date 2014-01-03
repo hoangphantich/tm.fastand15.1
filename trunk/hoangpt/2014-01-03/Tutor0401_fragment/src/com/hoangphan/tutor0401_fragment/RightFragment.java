042aa1df2191c58175a7ded50f1a84d5b0a37c10
phantichhoang <phantichhoang@4df6940f-bb5a-25f8-e46e-23d9f5565ca8>
2014-01-03 03:30:35 +0000

    Tutor

---
+++ trunk/hoangpt/2014-01-03/Tutor0401_fragment/src/com/hoangphan/tutor0401_fragment/RightFragment.java
@@ -0,0 +1,20 @@
+package com.hoangphan.tutor0401_fragment;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("NewApi")
+public class RightFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.right, container, false);
+	}
+	
+}
---
+++ trunk/hoangpt/2014-01-03/Tutor0402_menutask/src/com/hoangphan/tutor0402_menutask/WorkAdapter.java
@@ -0,0 +1,40 @@
+package com.hoangphan.tutor0402_menutask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class WorkAdapter extends ArrayAdapter<Work> {
+  Context app;
+  ArrayList<Work> list;
+
+  public WorkAdapter(Context context, int resource, List<Work> objects) {
+    super(context, resource, objects);
+    app = context;
+    list = (ArrayList<Work>) objects;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    View workView = convertView;
+    if(null == workView) {
+      workView = new WorkView(app);
+    }
+    
+    Work work = list.get(position);
+    if(null != work) {
+      WorkView row = (WorkView) workView;
+      row.workName.setText(work.getName());
+      row.workTime.setText(work.getHour() + ":" + work.getMinute());
+      row.checkBox.setChecked(work.isDone());
+    }
+    
+    return workView;
+  }
+  
+  
+}
