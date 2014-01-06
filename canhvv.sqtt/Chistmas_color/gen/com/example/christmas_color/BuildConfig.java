ff2ed04a16d0010096e99207a505fdb0cfab87d2
androidfastechmaster2412130900 <androidfastechmaster2412130900@gmail.com>
2014-01-06 07:06:42 +0000

    commit this project

--- canhvv.sqtt/Chistmas_color/gen/com/example/christmas_color/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.christmas_color;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- canhvv.sqtt/NangSG_Tpster/gen/com/example/nangsg_tipster/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.nangsg_tipster;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
