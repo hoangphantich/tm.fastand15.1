d7cc78a8fb0a492bf6a6e1c756d0d02fd8de1315
phantichhoang <phantichhoang@4df6940f-bb5a-25f8-e46e-23d9f5565ca8>
2014-01-15 04:32:09 +0000

    sample project

---
+++ trunk/hoangpt/2014-01-14/Lab0901_notifications/gen/com/hoangphan/tutor0901_notification/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hoangphan.tutor0901_notification;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
